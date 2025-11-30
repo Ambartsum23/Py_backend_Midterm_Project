6f62640f0e52b16b16f07ef124ceaa64290b4cf6
Nini Abuladze <niniabuladze72@gmail.com>
2025-11-30 14:54:43 +0400

    Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -142,37 +142,44 @@ def finished(word, guessed_letters, wrong, max_wrong):
     #თუ არც მოგებულია და არც წაგებული, მაშინ თამაში გრძელდება
     return False
 
-def play_hangman(max_wrong=None):
-    #თამაშის ციკლი
+def play_hangman():
     word = choose_word()
     guessed_letters = []
     wrong = 0
-    max_wrong = max_wrong if max_wrong else len(Hangman) - 1
+    max_wrong = 6
 
     while True:
-        #ნახატი
+        # ნახატი
         print(Hangman[wrong])
-
-        #სიტყვა
+        # სიტყვის ჩვენება
         show_word(word, guessed_letters)
-
-        #უკვე გამოცნობილი ასოების ჩვენება
+        # უკვე შეყვანილი ასოების ჩვენება
         if guessed_letters:
             print("Guessed letters:", " ".join(guessed_letters))
-
-        #შეყვანა
+        # შეყვანა
         guess = try_guess(guessed_letters)
         if not guess:
-            continue  # ცარიელი შეყვანა იგნორირდება
-
+            continue
         # განახლება
-        wrong = update_game(word, guess, guessed_letters, wrong)
-
-        # დასრულების შემოწმება
-        if finished(word, guessed_letters, wrong, max_wrong):
+        if guess not in word:
+            wrong += 1
+            print("Letter", guess, "is not in the word")
+        else:
+            print("Correct! The letter", guess, "is in the word")
+        #შეყვანილი ასოს შენახვა
+        guessed_letters.append(guess)
+        # გამარჯვების შემოწმება
+        won = all(letter in guessed_letters for letter in word)
+        if won:
+            print("Congrats! You won! The word was:", word)
+            break
+        # წაგების შემოწმება
+        if wrong == max_wrong:
+            print(Hangman[wrong])  # ბოლო ნახატი
+            print("You lose! The word was:", word)
             break
 
-play_hangman(max_wrong=len(Hangman) - 1)
+play_hangman()
 
 
 
